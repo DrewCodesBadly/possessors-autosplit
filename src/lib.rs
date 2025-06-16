bc6044aa0ebea57bdf817d16f0d9e6c3b65e1afa
DrewCodesBadly <dhruvak@outlook.com>
2025-06-15 21:29:27 -0400

    working

--- src/lib.rs
+++ src/lib.rs
@@ -4,37 +4,173 @@ extern crate alloc;
 #[global_allocator]
 static ALLOC: dlmalloc::GlobalDlmalloc = dlmalloc::GlobalDlmalloc;
 
-use asr::{future::next_tick, settings::Gui, Process};
+#[cfg(debug_assertions)]
+use alloc::format;
+use alloc::string::String;
+use asr::{future::next_tick, game_engine::unreal::{Module, UnrealPointer, Version}, print_message, settings::Gui, timer::{pause_game_time, resume_game_time, set_variable, split, start}, Address, Process};
 
 asr::async_main!(stable);
 asr::panic_handler!();
 
+
 #[derive(Gui)]
 struct Settings {
-    /// My Setting
+    /// Autostart
     #[default = true]
-    my_setting: bool,
-    // TODO: Change these settings.
+    start: bool,
+
+    /// Split on demo end
+    #[default = true]
+    demo_end: bool,
+
+    /// Split when collecting an ability
+    #[default = true]
+    abilities: bool,
 }
 
+// TODO: Optimize all of this instead of using the unreal functions.
 async fn main() {
-    // TODO: Set up some general state and settings.
     let mut settings = Settings::register();
-
-    asr::print_message("Hello, World!");
 
     loop {
         let process = Process::wait_attach("Pose-Win64-Shipping.exe").await;
         process
             .until_closes(async {
-                // TODO: Load some initial information from the process.
+                // Load initial info
+                let (main_module_address, _main_module_size) = process.get_module_range("Pose-Win64-Shipping.exe").unwrap();
+                #[cfg(debug_assertions)]
+                print_message("found main module");
+
+                // I think it's V5.5 but it works anyway. 5.5 not supported yet.
+                let module = Module::wait_attach(&process, Version::V5_4, main_module_address).await;
+
+                #[cfg(debug_assertions)]
+                print_message("Attached to UE game");
+
+                let world = module.g_world();
+                let player: UnrealPointer<5> = UnrealPointer::new(world, &["OwningGameInstance", "LocalPlayers"]);
+                let mut last_demo_end = 0;
+                let mut last_ability = 0;
+                let mut luca_has_legs = true;
+
+                #[cfg(debug_assertions)]
+                print_message(&format!("World address: {:?}", world));
+
+
                 loop {
                     settings.update();
+                    if let Some(world_obj) = module.get_g_world_uobject(&process) {
+                        let name = world_obj.get_fname::<25>(&process, &module);
+                        if let Ok(n) = name {
+                            if let Ok(s) = String::from_utf8(n.as_bytes().to_vec()) {
+                                set_variable("gworld name", &s);
+                            }
+                        }
+                    }
+                    
+                    if let Ok(player_location) = player.deref_offsets(&process, &module)
+                        .and_then(|addr| process.read::<u64>(addr)) {
+                        // INVALID, DEAD, ALIVE, RESPAWNING. Kinda pointless?
+                        // If you want something to happen when you die maybe?
+                        // let player_state: UnrealPointer<5> = UnrealPointer::new(Address::new(*array_start), &[
+                        //     "PlayerController",
+                        //     "Pawn",
+                        //     "CoreState",
+                        // ]);
+                        // set_variable("player state", &format!("{:?}", player_state.deref::<[u8; 64]>(&process, &module)));
 
-                    // TODO: Do something on every tick.
+                        // I'm just gonna use the ability unlock screen since I haven't tested
+                        // this.
+                        // let player_abilities: UnrealPointer<5> = UnrealPointer::new(Address::new(*player_location), &[
+                        //     "PlayerController",
+                        //     "Pawn",
+                        //     "DefaultUnlockedAbilities",
+                        // ]);
+                        //
+
+                        // ABP_PosePlayerPawn_C has some real fun stuff in there. HUUUUGE
+                        // class. of all things here i'm gonna try to use legless luca (lmao)
+                        // as a start autosplit.
+                        // leglessLuca is a bool in that class which, as you may guess,
+                        // represents whether or not luca has her legs. It's pretty safe to say
+                        // if the last read was nonexistent or false we are starting a new
+                        // game.
+                        let legless_luca: UnrealPointer<4> = UnrealPointer::new(Address::new(player_location), &[
+                            "PlayerController",
+                            "Pawn",
+                            "leglessLuca",
+                        ]);
+                        if legless_luca.deref::<u8>(&process, &module).ok().filter(|no_legs| *no_legs == 1).is_some() {
+                            if settings.start && luca_has_legs {
+                                luca_has_legs = false;
+                                start();
+                            } else {
+                                luca_has_legs = true;
+                            }
+                        } else {
+                            luca_has_legs = true;
+                        }
+                        
+                        let load_status: UnrealPointer<4> = UnrealPointer::new(Address::new(player_location), &[
+                            "PlayerController",
+                            "MyHUD",
+                            "LoadingScreen",
+                            "CurrentStatus",
+                        ]);
+                        // Is loading when this == 3
+                        if load_status.deref::<u8>(&process, &module).ok().filter(|s| *s == 3).is_some() {
+                            pause_game_time();
+                        } else {
+                            resume_game_time();
+                        }
+
+                        // These two work the same way.
+                        // They are either broken (The pointer to the screen is null) before
+                        // the screen has triggered once and loaded,
+                        // or they are a boolean 1 or 0, 1 when they're shown.
+                        let demo_end: UnrealPointer<4> = UnrealPointer::new(Address::new(player_location), &[
+                            "PlayerController",
+                            "MyHUD",
+                            "DemoEndScreen",
+                            "bIsActive",
+                        ]);
+                        // This logic is kinda jank but it should be fine
+                        if let Ok(b) = demo_end.deref::<u8>(&process, &module) {
+                            if settings.demo_end && b == 1 && last_demo_end == 0 {
+                                last_demo_end = 1;
+                                split();
+                            } else {
+                                last_demo_end = b;
+                            }
+                        } else {
+                            last_demo_end = 0;
+                        }
+
+                        let ability_unlock: UnrealPointer<4> = UnrealPointer::new(Address::new(player_location), &[
+                            "PlayerController",
+                            "MyHUD",
+                            "AbilityUnlockedScreen",
+                            "bIsActive",
+                        ]);
+                        if let Ok(b) = ability_unlock.deref::<u8>(&process, &module) {
+                            if settings.abilities && b == 1 && last_ability == 0 {
+                                last_ability = 1;
+                                split();
+                            } else {
+                                last_ability = b;
+                            }
+                        } else {
+                            last_ability = 0;
+                        }
+                    } else {
+                        luca_has_legs = true;
+                    }
+
+                    #[cfg(debug_assertions)]
+                    set_variable("luca_has_legs", &format!("{}", luca_has_legs));
+
                     next_tick().await;
                 }
-            })
-            .await;
+            }).await;
     }
 }
